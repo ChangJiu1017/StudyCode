5123e01ca3a6ea271da8766a9ddf5c9986469d50
changxinyu <changxinyu001@gmail.com>
2024-08-21 16:45:20 +0800

    【feature】change project config information

---
+++ build/CMakeFiles/StudyCode.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StudyCode.dir/src/main.cpp.obj"
+  "CMakeFiles/StudyCode.dir/src/main.cpp.obj.d"
+  "StudyCode.exe"
+  "StudyCode.exe.manifest"
+  "StudyCode.pdb"
+  "libStudyCode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StudyCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
